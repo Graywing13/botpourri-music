9ec84fd592661350dc5837fc81d5bfdddc7d005a
ss13 <chan.noreen609@gmail.com>
2021-05-26 14:41:47 -0700

    implemented queue (looping mechanism incomplete)

--- commands/music_cmd/play.js
+++ commands/music_cmd/play.js
@@ -7,14 +7,51 @@ module.exports = {
   usage: "<song url>",
   execute: async function(msg, serverQueue, args) {
 
+    // a private function
+    var play_next = function() {
+      if (!serverQueue.songs.length) {
+        serverQueue.playing = false;
+        return msg.channel.send("There are no more queued songs.");
+      }
+      const toPlay = serverQueue.songs.shift();
+
+      let dispatcher = connection.play(toPlay);
+
+      dispatcher.on('start', () => {
+        msg.channel.send("Playing `" + toPlay + "`");
+        serverQueue.playing = true;
+        if (serverQueue.queueLoop && !serverQueue.loop) {
+          serverQueue.songs.push(toPlay);
+        }
+      });
+
+      dispatcher.on('finish', () => {
+        msg.channel.send("Done playing `" + toPlay + "`");
+        if (serverQueue.loop) {
+          serverQueue.songs.unshift(toPlay);
+        } else if (serverQueue.queueLoop) {
+          serverQueue.songs.push(toPlay);
+        }
+        play_next();
+      });
+
+      // handle errors!
+      dispatcher.on('error', () => {
+        msg.channel.send("Error with `" + toPlay + "`");
+      });
+    }
+
     // #TODO: check if targetSong ends in a recognized format
     //        else look up args[all] on ytld-core and set targetSong to first found song. 
     //        let the user know which song botpourri found.  
     let targetSong = args[0];
     let connection = null; // +TODO figure out what this is
 
+    serverQueue.songs.push(targetSong);
+    msg.channel.send("Queued `" + targetSong + "`");
+
     // Join the same voice channel of the author of the message if not currently in a channel
-    if (!serverQueue.voiceChannel) {
+    if (!serverQueue.memberVoiceState) { // nothing playing / connected originally 
       if (msg.member.voice.channel) {
         serverQueue.memberVoiceState = msg.member.voice;
         connection = await serverQueue.memberVoiceState.channel.join();
@@ -25,32 +62,21 @@ module.exports = {
       } else {
         return msg.channel.send("Please join a voice channel :3");
       }
-    } else if (msg.member.voice.channel != serverQueue.memberVoiceState.channel) {
+    } else if (msg.member.voice.channel != serverQueue.memberVoiceState.channel) { // user in diff VC calls bot
       return msg.channel.send("You need to be in the same voice channel as " + + " to use this command dechu.");
-    } else {
+    } else if (serverQueue.playing) { // something is playing; we gonna add song to queue
+      // this is ok
+    } else { // all other cases i havent encountered yet. 
       return msg.channel.send("Error occured dechu.")
     }
-    
-    msg.channel.send("Playing `" + targetSong + "`");
 
     // // #DELETE Testing purposes
     // const connection = await msg.member.voice.channel.join().then(debugConnection => {
     //   debugConnection.on('debug', console.log);
     // });
-
-    let dispatcher = connection.play(targetSong);
-
-    dispatcher.on('start', () => {
-      msg.channel.send("Playing " + targetSong);
-    });
-
-    dispatcher.on('finish', () => {
-      msg.channel.send("Done playing " + targetSong);
-    });
-
-    // handle errors!
-    dispatcher.on('error', () => {
-      msg.channel.send("Cannot find " + targetSong);
-    });
+    
+    if (!serverQueue.playing) {
+      play_next();
+    }
   }
 }
