a90f6f2c8e03d4c6b8ca25fe9ed7f7d72e180e3a
ss13 <chan.noreen609@gmail.com>
2021-06-11 20:30:18 -0700

    added skip function

--- commands/music_cmd/skip.js
+++ commands/music_cmd/skip.js
@@ -1,9 +1,14 @@
 module.exports = { 
   name: 'skip',
+  alias: 'fs',
   description: 'skip',
   usage: "",
   requiresServerQueue: true,
-  execute(msg, requiresServerQueue, args) {
-    return msg.channel.send("skip!");
+  execute(msg, serverQueue, args) {
+    if (!msg.member.voice.channel) {
+      return msg.channel.send("You have to be in a channel to skip :3");
+    }
+    serverQueue.connection.dispatcher.end();
+    return msg.channel.send(":fast_forward: skip!");
   }
 }
